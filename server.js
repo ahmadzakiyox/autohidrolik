b42b1fce3b287953e3ffaad74da2dafc78759b4e
Ahmad Zaki <166486850+ahmadzakiyox@users.noreply.github.com>
2025-08-31 19:48:30 +0700

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const corsOptions = {
   },
   optionsSuccessStatus: 200 
 };
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
